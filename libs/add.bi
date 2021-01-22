6c57040786c7f8ffe41a863e93f818334d7aa426
lisbon <16432726+honeydatax@users.noreply.github.com>
2021-01-22 08:58:49 +0000

    As add.s ;fbc -lib add.o add.a;fbc add.bas

---
+++ libs/add.bi
@@ -0,0 +1,5 @@
+#inclib "add"
+extern "C"
+	declare function adds cdecl alias "adds"(a as integer,b as integer)as integer
+end extern
+
---
+++ libs/add.s
@@ -0,0 +1,15 @@
+	.global	adds
+adds:
+	str	fp, [sp, #-4]!
+	add	fp, sp, #0
+	sub	sp, sp, #12
+	str	r0, [fp, #-8]
+	str	r1, [fp, #-12]
+	ldr	r2, [fp, #-8]
+	ldr	r3, [fp, #-12]
+	add	r3, r2, r3
+	mov	r0, r3
+	add	sp, fp, #0
+	@ sp needed
+	ldr	fp, [sp], #4
+	bx	lr
